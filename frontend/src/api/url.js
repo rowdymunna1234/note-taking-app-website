7d5898356b649639f974ca3a460464df698fdada
rowdymunna1234 <anamitrachowdhury7@gmail.com>
2025-11-13 12:53:13 +0530

    Update url.js

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = axios.create({
-  baseURL: "https://note-taking-app-website-backend-1.onrender.com"
+  baseURL: "http://localhost:4001/api/v1/noteapp/"
 })
 
 export default BACKEND_URL
