69c70ab472431c48effd54c52bb0148d0ff0714e
rowdymunna1234 <anamitrachowdhury7@gmail.com>
2025-11-13 14:58:02 +0530

    url backend change

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = axios.create({
-  baseURL: "https://note-taking-app-website-frontend-latest.onrender.com"
+  baseURL: "https://note-taking-app-website-backend-newest.onrender.com"
 })
 
 export default BACKEND_URL
