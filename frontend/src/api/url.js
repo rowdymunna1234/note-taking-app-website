4ff729c82f0821c44a0f110d8884f5eed3c06beb
rowdymunna1234 <anamitrachowdhury7@gmail.com>
2025-11-15 02:08:55 +0530

    deploy new

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = axios.create({
-  baseURL: "http://localhost:4001/api/v1/noteapp",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:4001/api/v1/noteapp",
 })
 
 export default BACKEND_URL
